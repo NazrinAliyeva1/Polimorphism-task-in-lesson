fbd7bb97bf9a981f9fd46cf3266665d13a7ae413
NazrinAli1 <anazrin711@gmail.com>
2024-04-06 07:25:20 -0600

    Polimorphism

---
+++ Polimorphism-Learn in lesson/Interfaces/IDifference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorphism_Learn_in_lesson.Interfaces
+{
+    interface IDifference
+    {
+        float Difference(float num1, float num2);
+    }
+}
---
+++ Polimorphism-Learn in lesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using Polimorphism_Learn_in_lesson.Interfaces;
+using Polimorphism_Learn_in_lesson.Models;
+
+namespace Polimorphism_Learn_in_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDivide c = new Calculate();
+            Console.WriteLine(c.Divide(40, 2));
+
+            IAdd a = new Calculate();
+            Console.WriteLine(a.Add(25, 30));
+
+            IDifference d = new Calculate();
+            Console.WriteLine(d.Difference(10, 8));
+
+            IMultiply m = new Calculate();
+            Console.WriteLine(m.Multiple(17, 4));
+
+
+        }
+    }
+}
